2ae30ec70e56b5293ace0f7a67fd76394de2eab5
mesqueeb <mesqueeb@users.noreply.github.com>
2024-08-05 11:55:02 +0900

    chore: correctly update minimum platform support

--- SwiftDataSugar/Package.swift
+++ SwiftDataSugar/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "SwiftDataSugar",
-  platforms: [.macOS(.v13), .iOS(.v16), .visionOS(.v1)],
+  platforms: [.macOS(.v14), .iOS(.v17), .visionOS(.v1)],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
